a829f2247ff9bc1219b4e9970a7c7a0576688a6e
Sébastien LAMBRANCA <116831255+Lambseb@users.noreply.github.com>
2023-12-14 10:45:15 +0100

    Update Cv.jsx

--- src/pages/contact/Cv.jsx
+++ src/pages/contact/Cv.jsx
@@ -1,6 +1,6 @@
 import styles from "./cv.module.css";
 import { motion } from "framer-motion";
-import CvSeb from "/public/LAMBRANCA_Sebastien_CV.pdf";
+import CvSeb from "/public/LAMBRANCA_Sebastien_cv.pdf";
 function Cv() {
   return (
     <>
