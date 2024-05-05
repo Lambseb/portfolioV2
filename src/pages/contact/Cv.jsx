b0d02af5c4d919d3d70a0c9a8297355224d23ffc
Lambseb <lambranca.s@gmail.com>
2024-05-05 14:52:14 +0200

    update CV

--- src/pages/contact/Cv.jsx
+++ src/pages/contact/Cv.jsx
@@ -1,6 +1,6 @@
 import styles from "./cv.module.css";
 import { motion } from "framer-motion";
-import CvSeb from "/public/LAMBRANCA_Sebastien.pdf";
+import CvSeb from "/public/LAMBRANCA-Sebastien_CV.pdf";
 function Cv() {
   return (
     <>
